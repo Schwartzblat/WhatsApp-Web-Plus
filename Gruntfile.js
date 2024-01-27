2fe5ac181cc86558c63e9f3719ef284be8e2c423
Schwartzblat <alon.ponch@gmail.com>
2024-01-27 13:50:40 +0200

    Add setup for creating packed.js file

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        concat: {
+            options: {
+                banner: 'const main = () => {\n',
+                footer: '\n};\nmain();',
+                separator: '\n\n',
+            },
+            dist: {
+                src: [
+                    './scripts/module_raid.js',
+                    './scripts/process_renderable_messages.js',
+                    './scripts/process_edited_messages.js',
+                    './scripts/router.js',
+                ],
+                dest: './packed.js',
+            },
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.registerTask('default', ['concat']);
+};
