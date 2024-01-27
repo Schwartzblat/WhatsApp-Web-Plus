0c37a803f2a46f58bd572f00666a034895b4c817
yan <yanoshercohen@gmail.com>
2024-01-27 13:55:27 +0200

    added console.log to the messages

--- scripts/process_edited_messages.js
+++ scripts/process_edited_messages.js
@@ -4,6 +4,7 @@ const initializeEditMessageHook = () => {
     const originalProcessor = window.mR.modules[189865].processEditProtocolMsg;
     window.mR.modules[189865].processEditProtocolMsg = function () {
         arguments[0] = arguments[0].filter((message) => !handleEditedMessage(message));
+        console.log(message);
         return originalProcessor(...arguments);
     };
 };
