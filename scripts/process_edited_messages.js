9fd159b9b1df9b100bda43d7d32b62a024d20550
yan <yanoshercohen@gmail.com>
2024-01-27 13:53:46 +0200

    better code

--- scripts/process_edited_messages.js
+++ scripts/process_edited_messages.js
@@ -1,20 +1,9 @@
-const handle_edited_message = (message) => {
-    let should_ignore = false;
-    should_ignore |= true;
-    return should_ignore;
-}
+const handleEditedMessage = (message) => false;
 
-
-const initialize_edit_message_hook = () => {
-    const original_processor = window.mR.modules[189865].processEditProtocolMsg
+const initializeEditMessageHook = () => {
+    const originalProcessor = window.mR.modules[189865].processEditProtocolMsg;
     window.mR.modules[189865].processEditProtocolMsg = function () {
-        for (const [index, message] of Object.entries(arguments[0])) {
-            console.log(message);
-            const should_ignore = handle_edited_message(message);
-            if (should_ignore) {
-                arguments[0].splice(index, 1);
-            }
-        }
-        return original_processor(...arguments);
+        arguments[0] = arguments[0].filter((message) => !handleEditedMessage(message));
+        return originalProcessor(...arguments);
     };
 };
