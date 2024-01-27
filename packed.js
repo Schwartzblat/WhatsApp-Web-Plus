fcc663bc725c97128d8184ab4eb142bf102266e7
yan <yanoshercohen@gmail.com>
2024-01-27 13:41:33 +0200

    rewrite

--- packed.js
+++ packed.js
@@ -1,82 +1,45 @@
 const main = () => {
-const initialize_modules = () => {
-    window.mR = (() => {
+    const mR = (() => {
         const mObj = {};
         window.webpackChunkwhatsapp_web_client.push([
             ["moduleRaid"],
             {},
-            (e) => {
-                Object.keys(e.m).forEach((mod) => {
-                    mObj[mod] = e(mod);
-                });
-            }
+            (e) => Object.keys(e.m).forEach((mod) => mObj[mod] = e(mod))
         ]);
-        const get = (id) => {
-            return mObj[id];
-        };
-        const findModule = (query) => {
-            const results = [];
-            const modules = Object.keys(mObj);
-            modules.forEach((mKey) => {
-                const mod = mObj[mKey];
-                if (typeof query !== "function" && typeof query !== "string") {
-                    return;
-                }
-                if (typeof query === "function" && query(mod)) {
-                    results.push(mod);
-                    return;
-                }
-                for (const key in (mod === null || mod === void 0 ?
-                    void 0 :
-                    mod.default) || mod) {
-                    if (key === query) {
-                        results.push(mod);
-                    }
-                }
-            });
-            return results;
-        };
+
+        const get = (id) => mObj[id];
+        const findModule = (query) => Object.keys(mObj).filter(mKey => {
+            const mod = mObj[mKey];
+            return typeof query === "function" ? query(mod) : (mod?.default || mod)[query] !== undefined;
+        });
+
         return {
             modules: mObj,
-            findModule: findModule,
-            get: get
+            findModule,
+            get
         };
     })();
-    console.log('Modules have been loaded successfully!');
-};
 
+    console.log('The modules have been loaded successfully!');
 
-const view_once_handler = (message) => {
-    if (!message?.isViewOnce) {
-        return;
-    }
-    message.isViewOnce = false;
-}
+    const viewOnceHandler = (message) => message?.isViewOnce && (message.isViewOnce = false);
 
+    const handleMessage = (message) => viewOnceHandler(message);
 
-const handle_message = (message) => {
-    view_once_handler(message);
-}
-
-
-const initialize_message_hook = () => {
-    const original_processor = window.mR.modules[992321].processRenderableMessages
-    window.mR.modules[992321].processRenderableMessages = function () {
-        for (const message of arguments[0]) {
-            handle_message(message);
-        }
-        return original_processor(...arguments);
+    const initializeMessageHook = () => {
+        const originalProcessor = mR.modules[992321].processRenderableMessages;
+        mR.modules[992321].processRenderableMessages = (...args) => {
+            args[0].forEach((message) => handleMessage(message));
+            return originalProcessor(...args);
+        };
     };
+
+    const start = async () => {
+        initializeMessageHook();
+    };
+
+    console.log('WAPower loaded successfully!');
+    setTimeout(start, 5000);
 };
 
-
-const start = async () => {
-    initialize_modules();
-    initialize_message_hook();
-};
-
-console.log('WhatsApp-Plus loaded successfully!');
-setTimeout(start, 5000);
-
-};
 main();
