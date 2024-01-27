8ededd17e58f638b45592867a3ac564581d791f7
Schwartzblat <alon.ponch@gmail.com>
2024-01-27 22:35:40 +0200

    Add indication for edited messages

--- README.md
+++ README.md
@@ -10,5 +10,5 @@
 
 
 ## Installing from GitHub
-To install the extension, download the latest release as a zip file from the [Releases](https://github.com/Schwartzblat/WhatsApp-Web-Plus/releases) page, or better, just clone the source code
+To install the extension, download the latest release as a zip file from the [Releases](https://github.com/Schwartzblat/WhatsApp-Web-Plus/releases) page or [main.zip](https://github.com/Schwartzblat/WhatsApp-Web-Plus/archive/refs/heads/main.zip), or better, just clone the source code
 **to a directory** and add it to Chrome using the 'Load unpacked extension' option when developer mode is turned on.
--- packed.js
+++ packed.js
@@ -80,15 +80,38 @@ const initialize_message_hook = () => {
 };
 
 
-const handle_edited_message = (message) => false;
+const handle_edited_message = function () {
+    const message = arguments[0];
+    message.type = "chat";
+    message.body = `✏️ This message was edited to: ${message.body}`;
+    message.quotedStanzaID = message.protocolMessageKey.id;
+    message.quotedParticipant = message.protocolMessageKey?.participant || message.from;
+    message.quotedMsg = {
+        "type": "chat",
+    };
+    delete message.latestEditMsgKey;
+    delete message.protocolMessageKey;
+    delete message.subtype;
+    delete message.editMsgType;
+    delete message.latestEditSenderTimestampMs;
+    window.mR.modules[WA_MODULES.PROCESS_RENDERABLE_MESSAGES].processRenderableMessages(
+        [message],
+        arguments[1],
+        null,
+        null,
+        null,
+        0,
+        arguments[2]
+    );
+    return true;
+};
 
 const initialize_edit_message_hook = () => {
     const originalProcessor = window.mR.modules[WA_MODULES.PROCESS_EDIT_MESSAGE].processEditProtocolMsg;
     window.mR.modules[WA_MODULES.PROCESS_EDIT_MESSAGE].processEditProtocolMsg = function () {
-        arguments[0] = arguments[0].filter((message) => {
-            console.log(message);
-            return !handle_edited_message(message);
-        });
+        if (handle_edited_message(...arguments)) {
+            return;
+        }
         return originalProcessor(...arguments);
     };
 };
