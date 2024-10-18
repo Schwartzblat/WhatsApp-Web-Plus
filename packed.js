7fef8a1569f7e0b8e6e405ae4f2256218a8f5e0c
Schwartzblat <alon.ponch@gmail.com>
2024-10-18 13:02:23 +0300

    A little code refactor and naming before merging

--- packed.js
+++ packed.js
@@ -407,6 +407,14 @@ class HookReceipts extends Hook {
 
 
 class FullscreenHook extends Hook {
+    FULLSCREEN_CSS = `
+        div[id="app"]>div>div[tabindex="-1"] {
+            min-width: 100% !important;
+            height: 100% !important;
+            top: 0 !important;
+        }
+    `;
+
     constructor() {
         super();
         this.style = null;
@@ -417,11 +425,10 @@ class FullscreenHook extends Hook {
         if (this.is_registered) {
             return;
         }
-        
         super.register();
-        
-        this.applyFullscreen();
-        this.setupObserver();
+
+        this.apply_fullscreen();
+        this.setup_observer();
     }
 
     unregister() {
@@ -429,42 +436,35 @@ class FullscreenHook extends Hook {
             return;
         }
         super.unregister();
-        
+
         if (this.style && this.style.parentNode) {
             this.style.parentNode.removeChild(this.style);
         }
-        
+
         if (this.observer) {
             this.observer.disconnect();
         }
-        
+
         this.style = null;
         this.observer = null;
     }
 
-    applyFullscreen() {
+    apply_fullscreen() {
         const targetElement = document.querySelector('div[id="app"]>div>div[tabindex="-1"]');
         if (targetElement) {
-            const css = `
-                div[id="app"]>div>div[tabindex="-1"] {
-                    min-width: 100% !important;
-                    height: 100% !important;
-                    top: 0 !important;
-                }
-            `;
             if (!this.style) {
                 this.style = document.createElement('style');
                 document.head.appendChild(this.style);
             }
-            this.style.textContent = css;
+            this.style.textContent = this.FULLSCREEN_CSS;
         }
     }
 
-    setupObserver() {
+    setup_observer() {
         this.observer = new MutationObserver((mutations) => {
             for (let mutation of mutations) {
                 if (mutation.type === 'childList') {
-                    this.applyFullscreen();
+                    this.apply_fullscreen();
                 }
             }
         });
@@ -475,6 +475,7 @@ class FullscreenHook extends Hook {
         });
     }
 }
+
 
 const hooks = {
     view_once_media: new ProtobufHook(),
