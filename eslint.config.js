62ab8aaf506af1df924c01ef34aa7e9354b3aefa
Schwartzblat <alon.ponch@gmail.com>
2024-10-24 01:02:39 +0300

    another check

--- eslint.config.js
+++ eslint.config.js
@@ -1,48 +1,1 @@
-const globals = require('globals');
-
-const js = require('@eslint/js');
-
-module.exports = [
-    js.configs.recommended,
-    {
-        languageOptions: {
-            globals: {
-                ...globals.browser,
-                ...globals.jest,
-            },
-            parserOptions: {
-                'ecmaVersion': 'latest'
-            },
-        },
-        ignores: [
-            'build'
-        ],
-        rules: {
-            indent: [
-                'error',
-                4
-            ],
-            'linebreak-style': [
-                'error',
-                'windows'
-            ],
-            quotes: [
-                'error',
-                'single'
-            ],
-            semi: [
-                'error',
-                'always'
-            ],
-            'no-undef': 'off',
-            'no-unused-vars': 'off'
-        }
-    },
-    {
-        files: ['packed.js'],
-        rules: {
-            'no-unused-vars': 'error',
-            'no-undef': 'error'
-        }
-    }
-];
+const globals = require('globals');const js = require('@eslint/js');module.exports = [    js.configs.recommended,    {        languageOptions: {            globals: {                ...globals.browser,                ...globals.jest,            },            parserOptions: {                'ecmaVersion': 'latest'            },        },        ignores: [            'build'        ],        rules: {            indent: [                'error',                4            ],            'linebreak-style': [                'error',                'windows'            ],            quotes: [                'error',                'single'            ],            semi: [                'error',                'always'            ],            'no-undef': 'off',            'no-unused-vars': 'off'        }    },    {        files: ['packed.js'],        rules: {            'no-unused-vars': 'error',            'no-undef': 'error'        }    }];
