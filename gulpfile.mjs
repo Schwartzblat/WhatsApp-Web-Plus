4a7caa4de166de175296d7e0aa42836064e5d4a9
alon <alon.ponch@gmail.com>
2024-12-09 02:05:56 +0200

    Fix gulp and remove print

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -3,7 +3,6 @@ import zip from 'gulp-zip';
 import {deleteAsync, deleteSync} from 'del';
 
 const FILES = [
-    'icons/**/*',
     'background.js',
     'index.js',
     'manifest.json',
@@ -12,6 +11,10 @@ const FILES = [
     'popup.js',
     'popup.css',
 ];
+
+const IMAGE_FILES = [
+    'icons/**/*',
+]
 
 const BUILD_PATH = 'build';
 const EXTENSION_FILENAME = 'whatsapp-web-plus';
@@ -26,9 +29,14 @@ export function packed() {
         .pipe(gulp.dest(BUILD_PATH));
 }
 
+export function images() {
+    return gulp.src(IMAGE_FILES, { base: '.', encoding: false})
+        .pipe(gulp.dest(BUILD_PATH + '/' + EXTENSION_FILENAME));
+}
+
 export function unpacked() {
     return gulp.src(FILES, { base: '.' })
         .pipe(gulp.dest(BUILD_PATH + '/' + EXTENSION_FILENAME));
 }
 
-gulp.task('default', gulp.series(clean, unpacked, packed));
+gulp.task('default', gulp.series(clean, unpacked, images, packed));
