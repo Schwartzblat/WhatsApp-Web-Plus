2020753b4fd48ba666f2f0facfd64041dc3682b4
alon <alon.ponch@gmail.com>
2024-12-10 22:15:26 +0200

    Fix gulpfile again

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -4,6 +4,7 @@ import {deleteAsync, deleteSync} from 'del';
 
 const FILES = [
     'ExtPay.js',
+    'icons/**/*',
     'background.js',
     'index.js',
     'manifest.json',
@@ -13,31 +14,22 @@ const FILES = [
     'popup.css',
 ];
 
-const IMAGE_FILES = [
-    'icons/**/*',
-]
-
 const BUILD_PATH = 'build';
-const EXTENSION_FILENAME = 'whatsapp-web-plus'
+const EXTENSION_FILENAME = 'whatsapp-web-plus';
 
 export function clean() {
     return deleteAsync(BUILD_PATH);
 }
 
 export function packed() {
-    return gulp.src(FILES, { base: '.' })
+    return gulp.src(FILES, {base: '.', encoding: false})
         .pipe(zip(EXTENSION_FILENAME + '.zip'))
         .pipe(gulp.dest(BUILD_PATH));
 }
 
-export function images() {
-    return gulp.src(IMAGE_FILES, { base: '.', encoding: false})
-        .pipe(gulp.dest(BUILD_PATH + '/' + EXTENSION_FILENAME));
-}
-
 export function unpacked() {
-    return gulp.src(FILES, { base: '.' })
+    return gulp.src(FILES, {base: '.'})
         .pipe(gulp.dest(BUILD_PATH + '/' + EXTENSION_FILENAME));
 }
 
-gulp.task('default', gulp.series(clean, unpacked, images, packed));
+gulp.task('default', gulp.series(clean, unpacked, packed));
