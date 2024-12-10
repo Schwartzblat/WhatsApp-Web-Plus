db570ffeca9be0a22973bfe0356cd06bdfeccc49
alon <alon.ponch@gmail.com>
2024-12-10 22:59:06 +0200

    Update gulp and eslint

--- eslint.config.js
+++ eslint.config.js
@@ -23,7 +23,7 @@ module.exports = [
             ],
             'linebreak-style': [
                 'error',
-                'windows'
+                'unix'
             ],
             quotes: [
                 'error',
